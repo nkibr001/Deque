ac0cc04a055063bd5a973e570a9f8a99ac35ac56
nkibr001 <60450655+nkibr001@users.noreply.github.com>
2020-08-17 16:29:03 -0400

    Add files via upload

---
+++ Deque.java
@@ -0,0 +1,189 @@
+//6039168
+public class Deque
+{
+    final int SIZE;
+    int[]list;
+    int front;
+    int rear;
+    int count;
+    
+    public Deque()
+    {
+        SIZE = 10;
+        list=new int[SIZE];
+        front=0;
+        count=0;
+        rear=0;     
+        //initializes everything to 0 to start the new deque
+    }
+    
+    public Deque(int size)
+    {
+        SIZE = size;
+        list=new int[SIZE];
+        front=0;
+        count=0;
+        rear=0;
+        //initalizes everything to 0 to start the new deque
+    }
+    
+    public boolean isEmpty()
+    {
+        return count==0;
+        //under the condition that there are no elements in the deque
+    }
+    
+    public void push (int x)
+    {        
+        if(count>SIZE - 1)
+        {
+            System.out.println("Full!");
+            //once the count is larger than the size-1, it's officially full
+        }
+        
+        else if(isEmpty())
+        {
+            list[front]=x;
+            count++;
+            //if it's empty, element gets put to front
+        }
+        else if (front==0)
+        {
+            front=SIZE-1;
+            list[front]=x;
+            count++;
+            //the pointer for front moves at the end, and the element is located there at the 'front'
+        }
+        else
+        {
+            front--;
+            list[front]=x;
+            count++;
+            //element gets put to front in and pointer points to the left
+        }
+        //count increments for addition of element
+    }
+    
+    public int pop()
+    {
+        int x=getFront();
+        //gets element so it can be returned once it's popped out
+        if(isEmpty())
+        {
+            System.out.println("No element in deque!");
+            //you can't use pop if there's nothing in deque
+        }
+           
+        if(front==rear)
+        {
+            front=-1;
+            count--;
+            //the front is put to a different spot from the rear if it's at the same position
+            //thus removing the only element there
+        }
+        else
+        {
+            if(front==SIZE-1)
+            {
+                front=0;
+                //if pointer of front is at the end it points to the start
+            }
+            else
+            {
+                front=front+1;
+                //or if it's in a different position, moves pointer to the right
+            }
+            count--;
+            //one less element, thus count decrements
+        }
+        
+        return x;      
+    }
+    
+    public void inject(int x)
+    {
+        if(count>SIZE-1)
+        {
+            System.out.println("Full!");
+        }
+         //once the count is larger than the size, it's officially full
+
+        else if(isEmpty())
+        {
+            list[rear]=x;
+            count++;
+            //if it's empty, put at rear since it's the only element
+        }
+        else if(rear==SIZE-1)
+        {
+            rear=0;
+            list[rear]=x;
+            count++;
+            // if it's at the last position, pointer points to the start as the rear
+        }
+        else
+        {
+            rear=rear+1;
+            list[rear]=x;
+            count++;
+            //points to the position to the right as rear
+        }       
+        //count increments with additional element
+    }
+    
+    public int eject()
+    {
+        int x=getRear();
+        if(isEmpty())
+        {
+            System.out.println("No element in deque!");
+            //you can't eject if it's an empty deque
+        }
+        
+        else
+        { 
+            if(front==rear)
+        {
+            rear=-1;
+            //the front is put to a different spot from the rear if it's at the same position
+            //thus removing the remaining element
+        }
+        else if(rear==0)
+        {
+            rear=SIZE-1;
+            //if rear is at the front, the pointer points to the back
+        }
+        else
+        {
+            rear--;
+            //rear constantly changes pointer position 
+        }
+            count--;
+            //count decrements once element is removed
+        }
+        
+        return x;
+    }
+    
+    public int getFront()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Empty!");
+            //tells you it's empty, thus there can be no front
+        }
+        return list[front];
+        //returns value at front
+    }
+    
+    public int getRear()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Empty!");
+            //tels you it's empty, thus there can be rear
+        }
+        return list[rear];
+        //returns value at rear
+    }
+}
---
+++ Main.java
@@ -0,0 +1,65 @@
+//6039168
+public class Main
+{
+    public static void main (String args[])
+    {
+        Deque D1= new Deque();
+        System.out.println("Deque created!");
+        //creates new Deque with automatic size of 10
+        
+        System.out.println("Is it empty: " + D1.isEmpty());
+        //check if it's empty
+        
+        System.out.println("Eject!");
+        D1.eject();
+        //checks for case that one tries to eject from an empty deque
+        
+        System.out.println("Input 1"); 
+        D1.push(1);
+        //input of 1 entered
+        
+        System.out.println("Is it empty: " + D1.isEmpty());
+        //checks if it's empty
+        
+        System.out.println("Front: " + D1.getFront());
+        //checks front value
+        
+        System.out.println("Inputs of 2 and 3 entered!");
+        D1.push(2);
+        D1.inject(3);
+        System.out.println("Front: Should be 2: " + D1.getFront());
+        System.out.println("Rear: Should be 3: " + D1.getRear());
+        //puts 2 to front and checks and puts 3 to rear and checks
+        
+        System.out.println("Inputs 4-9 entered!");
+        for (int index = 4; index <= 9; index ++)
+        {
+            D1.inject(index);
+        }
+        //injects additional values
+        
+        System.out.println("Input of 10 entered!");
+        D1.push(10);
+        //puts 10 to front
+        
+        System.out.println("Input of 11 entered!");
+        D1.push(11);
+        //checks to see if additional values can be inputted
+        
+        
+        System.out.println("Front should be 10: " + D1.getFront());
+        //checks to see if 10 is in front
+        
+        System.out.println("End should be 9: " + D1.getRear());
+        //checks to see if 9 is in rear
+        
+        System.out.println("Value in front popped and value in back ejected!");
+        
+        System.out.println("Value popped " + D1.pop());
+        System.out.println("Front should be 2: " + D1.getFront());
+        System.out.println("Value ejected " + D1.eject());
+        System.out.println("Rear should be 8: " + D1.getRear());
+        
+  
+    }
+}
